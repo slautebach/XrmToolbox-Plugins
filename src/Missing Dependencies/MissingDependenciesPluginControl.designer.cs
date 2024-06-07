3ca8a127313eedcb6ee9092bd8ecd732234354e3
Shawn Lautebach <shawn.lautebach@mnp.ca>
2024-06-07 10:13:03 -0400

    Add support for parsing xml

--- src/Missing Dependencies/MissingDependenciesPluginControl.designer.cs
+++ src/Missing Dependencies/MissingDependenciesPluginControl.designer.cs
@@ -28,31 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tabMissingDependencies = new System.Windows.Forms.TabControl();
+            this.tabPageXml = new System.Windows.Forms.TabPage();
+            this.scintillaXml = new ScintillaNET.Scintilla();
+            this.buttonRefreshSolution = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxSolutions = new System.Windows.Forms.ComboBox();
             this.tsbClose = new System.Windows.Forms.ToolStripButton();
             this.tssSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.tsbSample = new System.Windows.Forms.ToolStripButton();
-            this.tabMissingDependencies = new System.Windows.Forms.TabControl();
-            this.tabPageXml = new System.Windows.Forms.TabPage();
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
             this.tabPageReport = new System.Windows.Forms.TabPage();
-            this.textBoxDependencyXml = new System.Windows.Forms.TextBox();
-            this.toolStripMenu.SuspendLayout();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.tabMissingDependencies.SuspendLayout();
             this.tabPageXml.SuspendLayout();
+            this.toolStripMenu.SuspendLayout();
+            this.tabPageReport.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // toolStripMenu
+            // tabMissingDependencies
             // 
-            this.toolStripMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tsbClose,
-            this.tssSeparator1,
-            this.tsbSample});
-            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
-            this.toolStripMenu.Name = "toolStripMenu";
-            this.toolStripMenu.Size = new System.Drawing.Size(629, 25);
-            this.toolStripMenu.TabIndex = 4;
-            this.toolStripMenu.Text = "toolStrip1";
+            this.tabMissingDependencies.Controls.Add(this.tabPageXml);
+            this.tabMissingDependencies.Controls.Add(this.tabPageReport);
+            this.tabMissingDependencies.Location = new System.Drawing.Point(3, 53);
+            this.tabMissingDependencies.Name = "tabMissingDependencies";
+            this.tabMissingDependencies.SelectedIndex = 0;
+            this.tabMissingDependencies.Size = new System.Drawing.Size(623, 306);
+            this.tabMissingDependencies.TabIndex = 5;
+            // 
+            // tabPageXml
+            // 
+            this.tabPageXml.Controls.Add(this.scintillaXml);
+            this.tabPageXml.Location = new System.Drawing.Point(4, 22);
+            this.tabPageXml.Name = "tabPageXml";
+            this.tabPageXml.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageXml.Size = new System.Drawing.Size(615, 280);
+            this.tabPageXml.TabIndex = 0;
+            this.tabPageXml.Text = "Missing Dependency Xml";
+            this.tabPageXml.UseVisualStyleBackColor = true;
+            // 
+            // scintillaXml
+            // 
+            this.scintillaXml.Lexer = ScintillaNET.Lexer.Xml;
+            this.scintillaXml.Location = new System.Drawing.Point(7, 6);
+            this.scintillaXml.Name = "scintillaXml";
+            this.scintillaXml.Size = new System.Drawing.Size(602, 268);
+            this.scintillaXml.TabIndex = 4;
+            this.scintillaXml.WrapMode = ScintillaNET.WrapMode.Word;
+            // 
+            // buttonRefreshSolution
+            // 
+            this.buttonRefreshSolution.Location = new System.Drawing.Point(245, 28);
+            this.buttonRefreshSolution.Name = "buttonRefreshSolution";
+            this.buttonRefreshSolution.Size = new System.Drawing.Size(116, 23);
+            this.buttonRefreshSolution.TabIndex = 3;
+            this.buttonRefreshSolution.Text = "Refresh Solutions";
+            this.buttonRefreshSolution.UseVisualStyleBackColor = true;
+            this.buttonRefreshSolution.Click += new System.EventHandler(this.buttonRefreshSolution_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Solution";
+            // 
+            // comboBoxSolutions
+            // 
+            this.comboBoxSolutions.FormattingEnabled = true;
+            this.comboBoxSolutions.Location = new System.Drawing.Point(60, 28);
+            this.comboBoxSolutions.Name = "comboBoxSolutions";
+            this.comboBoxSolutions.Size = new System.Drawing.Size(169, 21);
+            this.comboBoxSolutions.TabIndex = 1;
             // 
             // tsbClose
             // 
@@ -71,78 +124,95 @@
             // 
             this.tsbSample.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.tsbSample.Name = "tsbSample";
-            this.tsbSample.Size = new System.Drawing.Size(46, 22);
-            this.tsbSample.Text = "Try me";
+            this.tsbSample.Size = new System.Drawing.Size(75, 22);
+            this.tsbSample.Text = "Load Report";
             this.tsbSample.Click += new System.EventHandler(this.tsbSample_Click);
             // 
-            // tabMissingDependencies
+            // toolStripMenu
             // 
-            this.tabMissingDependencies.Controls.Add(this.tabPageXml);
-            this.tabMissingDependencies.Controls.Add(this.tabPageReport);
-            this.tabMissingDependencies.Location = new System.Drawing.Point(3, 28);
-            this.tabMissingDependencies.Name = "tabMissingDependencies";
-            this.tabMissingDependencies.SelectedIndex = 0;
-            this.tabMissingDependencies.Size = new System.Drawing.Size(623, 331);
-            this.tabMissingDependencies.TabIndex = 5;
-            // 
-            // tabPageXml
-            // 
-            this.tabPageXml.Controls.Add(this.textBoxDependencyXml);
-            this.tabPageXml.Location = new System.Drawing.Point(4, 22);
-            this.tabPageXml.Name = "tabPageXml";
-            this.tabPageXml.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageXml.Size = new System.Drawing.Size(615, 305);
-            this.tabPageXml.TabIndex = 0;
-            this.tabPageXml.Text = "Missing Dependency Xml";
-            this.tabPageXml.UseVisualStyleBackColor = true;
+            this.toolStripMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbClose,
+            this.tssSeparator1,
+            this.tsbSample});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Size = new System.Drawing.Size(629, 25);
+            this.toolStripMenu.TabIndex = 4;
+            this.toolStripMenu.Text = "toolStrip1";
+            this.toolStripMenu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStripMenu_ItemClicked);
             // 
             // tabPageReport
             // 
+            this.tabPageReport.Controls.Add(this.splitContainer1);
             this.tabPageReport.Location = new System.Drawing.Point(4, 22);
             this.tabPageReport.Name = "tabPageReport";
             this.tabPageReport.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageReport.Size = new System.Drawing.Size(615, 305);
+            this.tabPageReport.Size = new System.Drawing.Size(615, 280);
             this.tabPageReport.TabIndex = 1;
             this.tabPageReport.Text = "Dependency Report";
             this.tabPageReport.UseVisualStyleBackColor = true;
             // 
-            // textBoxDependencyXml
+            // splitContainer1
             // 
-            this.textBoxDependencyXml.Location = new System.Drawing.Point(6, 6);
-            this.textBoxDependencyXml.Multiline = true;
-            this.textBoxDependencyXml.Name = "textBoxDependencyXml";
-            this.textBoxDependencyXml.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxDependencyXml.Size = new System.Drawing.Size(603, 293);
-            this.textBoxDependencyXml.TabIndex = 0;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(609, 274);
+            this.splitContainer1.SplitterDistance = 203;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(196, 264);
+            this.listBox1.TabIndex = 0;
             // 
             // MissingDependenciesPluginControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tabMissingDependencies);
+            this.Controls.Add(this.buttonRefreshSolution);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.toolStripMenu);
+            this.Controls.Add(this.comboBoxSolutions);
             this.Name = "MissingDependenciesPluginControl";
             this.Size = new System.Drawing.Size(629, 362);
             this.Load += new System.EventHandler(this.On_Load);
             this.Resize += new System.EventHandler(this.On_Resize);
-            this.toolStripMenu.ResumeLayout(false);
-            this.toolStripMenu.PerformLayout();
             this.tabMissingDependencies.ResumeLayout(false);
             this.tabPageXml.ResumeLayout(false);
-            this.tabPageXml.PerformLayout();
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            this.tabPageReport.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.ToolStrip toolStripMenu;
-        private System.Windows.Forms.ToolStripButton tsbClose;
-        private System.Windows.Forms.ToolStripButton tsbSample;
-        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
         private System.Windows.Forms.TabControl tabMissingDependencies;
         private System.Windows.Forms.TabPage tabPageXml;
+        private System.Windows.Forms.Button buttonRefreshSolution;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxSolutions;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.ToolStripButton tsbSample;
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private ScintillaNET.Scintilla scintillaXml;
         private System.Windows.Forms.TabPage tabPageReport;
-        private System.Windows.Forms.TextBox textBoxDependencyXml;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
---
+++ src/Missing Dependencies/MissingDependency.cs
@@ -0,0 +1,367 @@
+﻿using Microsoft.Xrm.Sdk;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace slautebach.MissingDependencies
+{
+
+    public enum ComponentType
+    {
+        Unknown = 0,
+        AIConfiguration = 402,
+        AIProject = 401,
+        AIProjectType = 400,
+        Attachment = 35,
+        Attribute = 2,
+        AttributeLookupValue = 5,
+        AttributeMap = 47,
+        AttributePicklistValue = 4,
+        CanvasApp = 300,
+        ComplexControl = 64,
+
+
+        ConnectionRole = 63,
+
+
+        Connector_371 = 371,
+
+
+        Connector_372 = 372,
+
+
+        ContractTemplate = 37,
+
+
+        ConvertRule = 154,
+
+
+        ConvertRuleItem = 155,
+
+
+        CustomControl = 66,
+
+
+        CustomControlDefaultConfig = 68,
+
+
+        DataSourceMapping = 166,
+
+
+        DisplayString = 22,
+
+
+        DisplayStringMap = 23,
+
+
+        DuplicateRule = 44,
+
+
+        DuplicateRuleCondition = 45,
+
+
+        EmailTemplate = 36,
+
+
+        Entity = 1,
+
+
+        EntityAnalyticsConfiguration = 430,
+
+
+        EntityKey = 14,
+
+
+        EntityMap = 46,
+
+
+        EntityRelationship = 10,
+
+
+        EntityRelationshipRelationships = 12,
+
+
+        EntityRelationshipRole = 11,
+
+
+        EnvironmentVariableDefinition = 380,
+
+
+        EnvironmentVariableValue = 381,
+
+
+        FieldPermission = 71,
+
+
+        FieldSecurityProfile = 70,
+
+
+        Form = 24,
+
+
+        HierarchyRule = 65,
+
+
+        ImportMap = 208,
+
+
+        Index = 18,
+
+
+        KBArticleTemplate = 38,
+
+
+        LocalizedLabel = 7,
+
+
+        MailMergeTemplate = 39,
+
+
+        ManagedProperty = 13,
+
+
+        MobileOfflineProfile = 161,
+
+
+        MobileOfflineProfileItem = 162,
+
+
+        OptionSet = 9,
+
+
+        Organization = 25,
+
+
+        PluginAssembly = 91,
+
+
+        PluginType = 90,
+
+
+        Privilege = 16,
+
+
+        PrivilegeObjectTypeCode = 17,
+
+
+        Relationship = 3,
+
+
+        RelationshipExtraCondition = 8,
+
+
+        Report = 31,
+
+
+        ReportCategory = 33,
+
+
+        ReportEntity = 32,
+
+
+        ReportVisibility = 34,
+
+
+        RibbonCommand = 48,
+
+
+        RibbonContextGroup = 49,
+
+
+        RibbonCustomization = 50,
+
+
+        RibbonDiff = 55,
+
+
+        RibbonRule = 52,
+
+
+        RibbonTabToCommandMap = 53,
+
+
+        Role = 20,
+
+
+        RolePrivilege = 21,
+
+
+        RoutingRule = 150,
+
+
+        RoutingRuleItem = 151,
+
+
+        SavedQuery = 26,
+
+
+        SavedQueryVisualization = 59,
+
+
+        SDKMessage = 201,
+
+
+        SDKMessageFilter = 202,
+
+
+        SdkMessagePair = 203,
+
+
+        SDKMessageProcessingStep = 92,
+
+
+        SDKMessageProcessingStepImage = 93,
+
+
+        SdkMessageRequest = 204,
+
+
+        SdkMessageRequestField = 205,
+
+
+        SdkMessageResponse = 206,
+
+
+        SdkMessageResponseField = 207,
+
+
+        ServiceEndpoint = 95,
+
+
+        SimilarityRule = 165,
+
+
+        SiteMap = 62,
+
+
+        SLA = 152,
+
+
+        SLAItem = 153,
+
+
+        SystemForm = 60,
+
+
+        ViewAttribute = 6,
+
+
+        WebResource = 61,
+
+
+        WebWizard = 210,
+
+
+        Workflow = 29,
+
+        // String types
+        ConnectionReference = 1000000
+    }
+
+
+    public class Component
+    {
+
+        public Component(XElement element)
+        {
+            DisplayName = element.Attribute("displayName")?.Value;
+            Solution = element.Attribute("solution")?.Value;
+            Id = element.Attribute("id")?.Value;
+            strType = element.Attribute("type")?.Value.ToLower();
+
+            // find all additional id properties
+            var alternativeId = element.Attributes().FirstOrDefault(a => a.Name.LocalName.StartsWith("id."));
+            if (alternativeId != null)
+            {
+                Id = alternativeId.Value;
+            }
+        }
+
+        // Type
+        private string strType { get; set; }
+
+        public ComponentType Type
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(strType))
+                {
+                    return ComponentType.Unknown;
+                }
+                if (int.TryParse(strType, out var type))
+                {
+                    return (ComponentType)type;
+                }
+                if (strType == "connectionreference")
+                {
+                    return ComponentType.ConnectionReference;
+                }
+                throw new NotImplementedException($"Tye type: {strType}, has not been implemented");
+            }
+        }
+
+
+        // Display Name
+        public string DisplayName { get; set; }
+
+        public string Solution { get; set; }
+
+        public string Id { get; set; }
+
+        public Guid IdGuid
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Id))
+                {
+                    return Guid.Empty;
+                }
+                return Guid.Parse(Id);
+            }
+        }
+
+    }
+
+    public class MissingDependencies
+    {
+        private List<MissingDependency> missingDependencies = new List<MissingDependency>();
+
+        public void Add(XElement required, XElement dependent, IOrganizationService service)
+        {
+
+            var reqComponent = new Component(required);
+            var depComponent = new Component(dependent);
+
+            var existing = missingDependencies.FirstOrDefault(m => m.Required.Id == reqComponent.Id);
+            if (existing == null)
+            {
+                existing = new MissingDependency(reqComponent);
+                missingDependencies.Add(existing);
+            }
+            existing.Dependents.Add(depComponent);
+        }
+
+        public List<MissingDependency> Dependencies
+        {
+            get
+            {
+                return missingDependencies;
+            }
+        }
+    }
+
+    public class MissingDependency
+    {
+        public MissingDependency(Component required)
+        {
+            Required = required;
+        }
+
+        public Component Required { get; set; }
+        public List<Component> Dependents { get; set; }
+    }
+}
